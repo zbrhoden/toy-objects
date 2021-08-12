78888cd2acc1fbdbbe4ab14b9851fe8f653a87b2
Blake Rhoden <zbrhoden@gmail.com>
2021-08-12 13:20:52 -0500

    toy-objects

---
+++ leonids-toys.js
@@ -0,0 +1,33 @@
+const phones = [
+    {
+        id: 1,
+        name: "iPhone",
+        maker: "Apple",
+        operatingSystem: "iOS",
+        price: 900,
+        weight: 1.2
+    },
+    {
+        id: 2,
+        name: "Galaxy",
+        maker: "Samsung",
+        operatingSystem: "Android",
+        price: 600,
+        weight: 1.4
+    }
+]
+
+const pixel = {
+    id: 3,
+    name: "Pixel",
+    maker: "Google",
+    operatingSystem: "Android",
+    price: 750,
+    weight: 1.3
+}
+
+phones.push(pixel)
+
+for (const phone of phones) {
+    console.log(`Price is $${phone.price}`)
+}
